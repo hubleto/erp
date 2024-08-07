632e1c83a6aab9d3bdfd230a52e8074c13ec64d7
Dusan <dusan.daniska@wai.sk>
2024-08-07 13:48:00 +0200

    test env

--- scripts/test-env/rebuild.sh
+++ scripts/test-env/rebuild.sh
@@ -6,10 +6,10 @@ export COMPOSER="composer-test-env.json"
 composer install
 composer update
 
-npm --prefix ~/ceremonycrm.com/sub/test-libs/ADIOS install ~/ceremonycrm.com/sub/test-libs/ADIOS/
+npm --prefix /var/www/html/ceremonycrm/libs/ADIOS install /var/www/html/ceremonycrm/libs/ADIOS/
 
 npm i
 npm run build
 
-rm -r ~/ceremonycrm.com/sub/test-app/app/bin
-cp -r ~/ceremonycrm.com/sub/test-app/src/publ ~/ceremonycrm.com/sub/test-app/app/bin
+rm -r /var/www/html/ceremonycrm/app/app/bin
+cp -r /var/www/html/ceremonycrm/app/src/publ /var/www/html/ceremonycrm/app/app/bin

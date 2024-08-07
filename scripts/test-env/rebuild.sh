8d7e0e661f9724f88aa71e47c0cd4f0272b180f2
Dusan <dusan.daniska@wai.sk>
2024-08-07 12:03:27 +0200

    test env

--- scripts/test-env/rebuild.sh
+++ scripts/test-env/rebuild.sh
@@ -1,8 +1,16 @@
 #/usr/bin/sh
 
-env COMPOSER=composer-test-env.json
+composer config --global use-parent-dir true
+export COMPOSER="composer-test-env.json"
+
 composer install
 composer update
 
+npm --prefix ~/ceremonycrm.com/sub/test-libs/ADIOS install ~/ceremonycrm.com/sub/test-libs/ADIOS/
+
 npm i
 npm run build
+
+if [ ! -d ~/ceremonycrm.com/sub/test-app/app/bin ]; then
+  ln -sf ~/ceremonycrm.com/sub/test-app/src/publ ~/ceremonycrm.com/sub/test-app/app/bin
+fi

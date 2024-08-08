0aa102de4fdc2ea7d8d2a58f17fda1beb9946dda
Richard O. <73641163+rindo789@users.noreply.github.com>
2024-08-08 17:30:01 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
     "./src/**/*.{html,js,twig,tsx}",
     "./vendor/wai-blue/adios/**/*.{tsx,twig}",

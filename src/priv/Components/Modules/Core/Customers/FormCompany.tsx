68ec0974d8a73aed968053730b902b7c81e25ee8
Richard O. <73641163+rindo789@users.noreply.github.com>
2024-09-29 18:56:07 +0200

    vymazávanie billing accounts

--- src/priv/Components/Modules/Core/Customers/FormCompany.tsx
+++ src/priv/Components/Modules/Core/Customers/FormCompany.tsx
@@ -285,7 +285,7 @@ export default class FormCompany<P, S> extends Form<
             <div className="card-body">
               <div className="list">
 
-                {R.BILLING_ACCOUNTS ? R.BILLING_ACCOUNTS.map((input, key) => {
+                {R.BILLING_ACCOUNTS && R.BILLING_ACCOUNTS.length > 0 ? R.BILLING_ACCOUNTS.map((input, key) => {
                   var servicesString = "";
 
                   if (input?.SERVICES) {
@@ -359,10 +359,6 @@ export default class FormCompany<P, S> extends Form<
                                       BILLING_ACCOUNTS: { [key]: {description: value} },
                                     });
                                   }}/>
-                                {/* <button className="btn btn-danger" onClick = {() => {
-                                  R.BILLING_ACCOUNTS[key]._toBeDeleted_ = true;
-                                  this.setState({ record: R });
-                                }}><span className="icon"><i className="fas fa-trash"></i></span></button> */}
                               </div>
                             </FormInput>
                           </div>
@@ -370,62 +366,86 @@ export default class FormCompany<P, S> extends Form<
 
                         {this.state.highlightIdBussinessAccounts == input.id ?
                           <div className="card mx-2 mb-2">
-                          <div className="card-header text-sm">Services connected to the Billing Account</div>
-                          <div className="card-body">
-                            <InputTable
-                              uid={this.props.uid + "_table_services_input"}
-                              {...this.getDefaultInputProps()}
-                              value={R.BILLING_ACCOUNTS[key].SERVICES ?? null}
-                              /* isInlineEditing={this.state.isInlineEditingBillingAccounts} */
-                              onChange={(value: any) => {
-                                this.updateRecord({
-                                  BILLING_ACCOUNTS: { [key]: {SERVICES: value}
+                            <div className="card-header text-sm">Services connected to the Billing Account</div>
+                            <div className="card-body">
+                              <InputTable
+                                uid={this.props.uid + "_table_services_input"}
+                                {...this.getDefaultInputProps()}
+                                value={R.BILLING_ACCOUNTS[key].SERVICES ?? null}
+                                /* isInlineEditing={this.state.isInlineEditingBillingAccounts} */
+                                onChange={(value: any) => {
+                                  this.updateRecord({
+                                    BILLING_ACCOUNTS: { [key]: {SERVICES: value}
+                                    },
+                                  });
+                                }}
+                              >
+                                <TableBillingAccountServices
+                                  uid={this.props.uid + "_table_services"}
+                                  context="Hello World"
+                                  descriptionSource="props"
+                                  description={{
+                                    permissions: {
+                                      canDelete: true,
+                                      canCreate: true,
+                                      canRead: true,
+                                      canUpdate: true,
+                                    },
+                                    columns: {
+                                      id_service: {
+                                        type: "lookup",
+                                        title: "Service Name",
+                                        model: "CeremonyCrmApp/Modules/Core/Services/Models/Service",
+                                      },
+                                    },
+                                  }}
+                                ></TableBillingAccountServices>
+                              </InputTable>
+                              {this.state.isInlineEditing ? (
+                                <a
+                                  role="button"
+                                  onClick={() => {
+                                    if (!R.BILLING_ACCOUNTS[key].SERVICES) R.BILLING_ACCOUNTS[key].SERVICES = [];
+                                    R.BILLING_ACCOUNTS[key].SERVICES.push({
+                                      id_billing_account: { _useMasterRecordId_: true },
+                                    });
+                                    this.setState({ record: R });
+                                  }}>
+                                  + Connect another service
+                                </a>
+                              ) : null}
+                            </div>
+                          </div>
+                        : null}
+                        {this.state.highlightIdBussinessAccounts == input.id && this.state.isInlineEditing ?
+                          <div className="mx-2 mb-2 flex flex-row justify-end">
+                            <button
+                              className="btn btn-danger text-sm"
+                              onClick={() => {
+                                request.get(
+                                  'api/record/delete',
+                                  {
+                                    model: 'CeremonyCrmApp/Modules/Core/Billing/Models/BillingAccount',
+                                    id: input.id,
                                   },
-                                });
+                                  (data: any) => {
+                                    if (data.status == true || data.id == 0) {
+                                      R.BILLING_ACCOUNTS.splice(key, 1);
+                                      this.setState({record: R});
+                                    }
+                                  }
+                                );
                               }}
                             >
-                              <TableBillingAccountServices
-                                uid={this.props.uid + "_table_services"}
-                                context="Hello World"
-                                descriptionSource="props"
-                                description={{
-                                  permissions: {
-                                    canDelete: true,
-                                    canCreate: true,
-                                    canRead: true,
-                                    canUpdate: true,
-                                  },
-                                  columns: {
-                                    id_service: {
-                                      type: "lookup",
-                                      title: "Service Name",
-                                      model: "CeremonyCrmApp/Modules/Core/Services/Models/Service",
-                                    },
-                                  },
-                                }}
-                              ></TableBillingAccountServices>
-                            </InputTable>
-                            {this.state.isInlineEditing ? (
-                              <a
-                                role="button"
-                                onClick={() => {
-                                  if (!R.BILLING_ACCOUNTS[key].SERVICES) R.BILLING_ACCOUNTS[key].SERVICES = [];
-                                  R.BILLING_ACCOUNTS[key].SERVICES.push({
-                                    id_billing_account: { _useMasterRecordId_: true },
-                                  });
-                                  console.log(R.BILLING_ACCOUNTS[key].SERVICES);
-                                  this.setState({ record: R });
-                                }}>
-                                + Connect another service
-                              </a>
-                            ) : null}
+                              <span className="icon"><i className="fas fa-trash-alt"></i></span>
+                              <span className="text">Delete Billing Account</span>
+                            </button>
                           </div>
-                        </div>
                         : null}
                       </div>
                     </>
                   )
-                }): null}
+                }): <span className="text-sm p-1">No Billing Accounts</span> }
               </div>
 
               {this.state.isInlineEditing ? (

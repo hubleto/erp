e68007ecf42a67b676ccf728d48542b3fd692ae9
Dusan <dusan.daniska@wai.sk>
2025-01-22 09:59:07 +0100

    check arguments in app test cli command

--- src/cli/Agent/App/Test.php
+++ src/cli/Agent/App/Test.php
@@ -9,17 +9,21 @@ class Test extends \HubletoMain\Cli\Agent\Command
     $appClass = $this->arguments[3] ?? '';
     $test = $this->arguments[4] ?? '';
 
-    $this->main->testMode = true;
+    if (empty($appClass) || empty($test)) {
+      $this->cli->white("Usage: php hubleto app test <appClass> <testName>\n");
+    } else {
+      $this->main->testMode = true;
 
-    try {
-      $appManager = new \HubletoMain\Core\AppManager($this->main);
-      $appManager->setCli($this->cli);
-      $appManager->testApp($appClass, $test);
-      $this->cli->cyan("✓ {$appClass} passed successfully test '{$test}'.\n");
-    } catch (\Throwable $e) {
-      $this->cli->red("✕ {$appClass} test failed.\n");
-      $this->cli->red($e->getMessage() . "\n");
-      $this->cli->red($e->getTraceAsString() . "\n");
+      try {
+        $appManager = new \HubletoMain\Core\AppManager($this->main);
+        $appManager->setCli($this->cli);
+        $appManager->testApp($appClass, $test);
+        $this->cli->cyan("✓ {$appClass} passed successfully test '{$test}'.\n");
+      } catch (\Throwable $e) {
+        $this->cli->red("✕ {$appClass} test failed.\n");
+        $this->cli->red($e->getMessage() . "\n");
+        $this->cli->red($e->getTraceAsString() . "\n");
+      }
     }
   }
 }

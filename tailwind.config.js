19b94fafaa98fff945742b60d6d5085d0e3392a7
Dusan <dusan.daniska@wai.sk>
2025-02-11 15:23:48 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,25 +14,6 @@ module.exports = {
     'adios-lookup__value-container',
     'adios-lookup__input',
   ],
-  // theme: {
-  //   fontFamily: {
-  //     sans: ['Fredoka-Regular', 'sans-serif'],
-  //     serif: ['Merriweather', 'serif'],
-  //   },
-  //   extend: {
-  //     colors: {
-  //       'primary': '#008000',
-  //       'secondary': '#7FB562', // povodna oranzova
-  //     },
-  //     spacing: {
-  //       '8xl': '96rem',
-  //       '9xl': '128rem',
-  //     },
-  //     borderRadius: {
-  //       '4xl': '2rem',
-  //     }
-  //   }
-  // },
   plugins: [],
 }
 

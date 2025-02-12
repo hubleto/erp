467fc334896ee4f0ea9ad76c1d4de3263cd4e159
Michal Blava <michal.blava@gmail.com>
2025-02-12 11:42:46 +0100

    pre inited state

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php
 
-ini_set('html_errors', "1");
-ini_set('error_prepend_string', "<pre style='color: #333; font-face:monospace; font-size:8pt;'>");
-ini_set('error_append_string', "</pre>");
-
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL ^ E_NOTICE ^ E_WARNING);
-
-define('_ADIOS_ID', 'HubletoMain-');
-
-// load configs
-require_once("/var/www/hubleto/ConfigApp.php");
-require_once(__DIR__ . "/ConfigEnv.php");
-
-// load autoloaders
-require("/var/www/hubleto/vendor/autoload.php");
-
-// load entry class
-require("/var/www/hubleto/src/Main.php");
-
-// render
-$main = new HubletoMain($config);
-echo $main->render();
+echo "Your Ceremony has not been initialized yet. Run `php ceremony init`.";
+echo "Your Hubleto has not been initialized yet. Run `php hubleto init`.";

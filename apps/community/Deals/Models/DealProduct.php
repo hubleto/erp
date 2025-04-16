0af3675679d5e60adc0ce3456323876175cf2f7e
Richard O. <73641163+rindo789@users.noreply.github.com>
2025-04-16 13:08:55 +0200

    another merge fix after eloquent refactor

--- apps/community/Deals/Models/DealProduct.php
+++ apps/community/Deals/Models/DealProduct.php
@@ -12,7 +12,7 @@ use \ADIOS\Core\Db\Column\Decimal;
 class DealProduct extends \HubletoMain\Core\Model
 {
   public string $table = 'deal_products';
-  public string $eloquentClass = RecordManagers\DealProduct::class;
+  public string $recordManagerClass = RecordManagers\DealProduct::class;
   public ?string $lookupSqlValue = '{%TABLE%}.id_product';
 
   public array $relations = [

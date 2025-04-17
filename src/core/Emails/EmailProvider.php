b54430e2172104e3bb14baba69387caf4cf9961a
Dusan <dusan.daniska@wai.sk>
2025-04-17 15:26:27 +0200

    ui updates

--- src/core/Emails/EmailProvider.php
+++ src/core/Emails/EmailProvider.php
@@ -25,6 +25,14 @@ class EmailProvider
 
   public function sendEmail($to, $subject, $body, $fromName = 'Hubleto Team')
   {
+    if (!class_exists(PHPMailer::class)) {
+      throw new \Exception('PHPMailer is required to send emails. Run `composer require phpmailer/phpmailer` to install it.');
+    }
+
+    if (empty($this->smtpHost) || empty($this->smtpUsername) || empty($this->smtpPassword) || empty($this->smtpEncryption) || empty($this->smtpPort)) {
+      throw new \Exception('SMTP is not properly configured. Cannot send emails.');
+    }
+
     $mail = new PHPMailer(true);
     try {
       $mail->isSMTP();

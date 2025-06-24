3b3a4cc6d183db643ca2b99abeaad4f3bd35b584
Dusan <dusan.daniska@wai.sk>
2025-06-24 21:32:43 +0200

    sidebarGroup

--- apps/community/Desktop/Controllers/Desktop.php
+++ apps/community/Desktop/Controllers/Desktop.php
@@ -28,6 +28,16 @@ class Desktop extends \HubletoMain\Core\Controllers\Controller
 
     $this->viewParams['appsInSidebar'] = $appsInSidebar;
     $this->viewParams['activatedApp'] = $activatedApp;
+    $this->viewParams['sidebarGroups'] = [
+      'basic' => [ 'title' => $this->translate('Home'), 'icon' => 'fas fa-home' ],
+      'crm' => [ 'title' => $this->translate('CRM'), 'icon' => 'fas fa-users-viewfinder' ],
+      'finance' => [ 'title' => $this->translate('Finance'), 'icon' => 'fas fa-credit-card' ],
+      'supply-chain' => [ 'title' => $this->translate('Supply chain'), 'icon' => 'fas fa-truck' ],
+      'reporting' => [ 'title' => $this->translate('Reporting'), 'icon' => 'fas fa-chart-line' ],
+      'settings' => [ 'title' => $this->translate('Settings'), 'icon' => 'fas fa-cog' ],
+      'help' => [ 'title' => $this->translate('Help'), 'icon' => 'fas fa-life-ring' ],
+      'custom' => [ 'title' => $this->translate('Custom'), 'icon' => 'fas fa-puzzle-piece' ],
+    ];
 
     $appMenu = $this->main->apps->community('Desktop')->appMenu;
     $this->viewParams['appMenu'] = $appMenu->getItems();

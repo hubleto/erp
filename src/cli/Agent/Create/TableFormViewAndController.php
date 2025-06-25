f98b4edb8f541fd953f0cbd27797319f801d2cff
Dusan <dusan.daniska@wai.sk>
2025-06-25 08:03:31 +0200

    hubleto create mvc

--- src/cli/Agent/Create/TableFormViewAndController.php
+++ src/cli/Agent/Create/TableFormViewAndController.php
@@ -8,8 +8,8 @@ class TableFormViewAndController extends \HubletoMain\Cli\Agent\Command
   public function run(): void
   {
     $appNamespace = (string) ($this->arguments[3] ?? '');
-    $model = (string) ($this->arguments[5] ?? '');
-    $force = (bool) ($this->arguments[6] ?? false);
+    $model = (string) ($this->arguments[4] ?? '');
+    $force = (bool) ($this->arguments[5] ?? false);
 
     $this->main->apps->init();
 
@@ -22,8 +22,16 @@ class TableFormViewAndController extends \HubletoMain\Cli\Agent\Command
 
     $appFolder = $app->rootFolder;
 
-    if (is_file($appFolder . '/Models/' . $model . '.php') && !$force) {
-      throw new \Exception("Model '{$model}' already exists in app '{$appNamespace}'. Use 'force' to overwrite existing files.");
+    if (
+      (
+        is_file($appFolder . '/Components/Table' . $model . '.tsx')
+        || is_file($appFolder . '/Components/Form' . $model . '.tsx')
+        || is_file($appFolder . '/Controllers/' . $controller . '.php')
+        || is_file($appFolder . '/Views/' . $view . '.php')
+      )
+      && !$force
+    ) {
+      throw new \Exception("Some of the MVC files for mode '{$model}' already exist in app '{$appNamespace}'. Use 'force' to overwrite existing files.");
     }
 
     $tplFolder = __DIR__ . '/../../../code_templates/snippets';
@@ -32,8 +40,13 @@ class TableFormViewAndController extends \HubletoMain\Cli\Agent\Command
     $controller = $model . 's'; // using plural for controller managing the records in the model
     $view = $model . 's'; // using plural for view managing the records in the model
 
+    $appNamespaceForwardSlash = str_replace('\\', '/', $appNamespace);
+    $appNamespaceDoubleBackslash = str_replace('/', '\\\\', $appNamespaceForwardSlash);
+
     $tplVars = [
       'appNamespace' => $appNamespace,
+      'appNamespaceForwardSlash' => $appNamespaceForwardSlash,
+      'appNamespaceDoubleBackslash' => $appNamespaceDoubleBackslash,
       'model' => $model,
       'sqlTable' => strtolower($model),
       'controller' => $controller,
@@ -43,8 +56,8 @@ class TableFormViewAndController extends \HubletoMain\Cli\Agent\Command
     if (!is_dir($appFolder . '/Components')) mkdir($appFolder . '/Components');
     if (!is_dir($appFolder . '/Controllers')) mkdir($appFolder . '/Controllers');
     if (!is_dir($appFolder . '/Views')) mkdir($appFolder . '/Views');
-    file_put_contents($appFolder . '/Components/Table' . $model . '.tsx', $this->main->twig->render('@snippets/ComponentTable.tsx.twig', $tplVars));
-    file_put_contents($appFolder . '/Components/Form' . $model . '.tsx', $this->main->twig->render('@snippets/ComponentForm.tsx.twig', $tplVars));
+    file_put_contents($appFolder . '/Components/Table' . $model . '.tsx', $this->main->twig->render('@snippets/Components/Table.tsx.twig', $tplVars));
+    file_put_contents($appFolder . '/Components/Form' . $model . '.tsx', $this->main->twig->render('@snippets/Components/Form.tsx.twig', $tplVars));
     file_put_contents($appFolder . '/Controllers/' . $controller . '.php', $this->main->twig->render('@snippets/Controller.php.twig', $tplVars));
     file_put_contents($appFolder . '/Views/' . $view . '.php', $this->main->twig->render('@snippets/View.twig.twig', $tplVars));
 

ee41aad6db7e5e1308ba4241a961510087dc2f1b
Dusan <dusan.daniska@wai.sk>
2025-08-27 12:30:16 +0200

    improved formTask

--- apps/Tasks/Components/FormTask.tsx
+++ apps/Tasks/Components/FormTask.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import HubletoForm, { HubletoFormProps, HubletoFormState } from '@hubleto/react-ui/ext/HubletoForm';
 import PipelineSelector from '@hubleto/apps/Pipeline/Components/PipelineSelector';
 import TableActivities from '@hubleto/apps/Worksheets/Components/TableActivities';
+import FormInput from '@hubleto/react-ui/core/FormInput';
 
 interface FormTaskProps extends HubletoFormProps { }
 interface FormTaskState extends HubletoFormState { }
@@ -76,6 +77,28 @@ export default class FormTask<P, S> extends HubletoForm<FormTaskProps, FormTaskS
         return <>
           <div className='w-full flex gap-2 flex-col md:flex-row'>
             <div className='flex-1 border-r border-gray-100'>
+              <div className='flex gap-2'>
+                {R.DEALS && R.DEALS.length > 0 ?
+                  <FormInput title={"Projects"}>{R.DEALS.map((item, key) => {
+                    return (item.DEAL ? <a
+                      key={key}
+                      className='badge'
+                      href={globalThis.main.config.projectUrl + '/deals/' + item.DEAL.id}
+                      target='_blank'
+                    >{item.DEAL.identifier}</a> : '#');
+                  })}</FormInput>
+                : null}
+                {R.PROJECTS && R.PROJECTS.length > 0 ?
+                  <FormInput title={"Projects"}>{R.PROJECTS.map((item, key) => {
+                    return (item.PROJECT ? <a
+                      key={key}
+                      className='badge'
+                      href={globalThis.main.config.projectUrl + '/projects/' + item.PROJECT.id}
+                      target='_blank'
+                    >{item.PROJECT.identifier}</a> : '#');
+                  })}</FormInput>
+                : null}
+              </div>
               {this.inputWrapper('identifier')}
               {this.inputWrapper('title')}
               {this.inputWrapper('description')}

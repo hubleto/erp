20d03fb8a7a664b8aaa144b2ba4ef2a3484afceb
Michal Blava <michal.blava@gmail.com>
2025-09-18 12:48:16 +0200

    added relationships for inventory

--- apps/Inventory/Models/RecordManagers/Inventory.php
+++ apps/Inventory/Models/RecordManagers/Inventory.php
@@ -4,15 +4,27 @@ namespace Hubleto\App\Community\Inventory\Models\RecordManagers;
 
 use Hubleto\App\Community\Auth\Models\RecordManagers\User;
 
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Hubleto\App\Community\Products\Models\RecordManagers\Product;
+use Hubleto\App\Community\Warehouses\Models\RecordManagers\Location;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Inventory extends \Hubleto\Erp\RecordManager
 {
   public $table = 'inventory';
 
-  public function MANAGER(): BelongsTo
+  public function PRODUCT(): HasOne
   {
-    return $this->belongsTo(User::class, 'id_manager', 'id');
+    return $this->hasOne(Product::class, 'id', 'id_product');
+  }
+
+  public function STATUS(): HasOne
+  {
+    return $this->hasOne(Status::class, 'id', 'id_status');
+  }
+
+  public function LOCATION(): HasOne
+  {
+    return $this->hasOne(Location::class, 'id', 'id_location');
   }
 
 }

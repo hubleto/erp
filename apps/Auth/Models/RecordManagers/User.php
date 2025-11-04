17b510f937438685ac9badf5b20735ddefcf3e2c
Dusan <dusan.daniska@wai.sk>
2025-11-04 22:41:33 +0100

    improved sidebar UI

--- apps/Auth/Models/RecordManagers/User.php
+++ apps/Auth/Models/RecordManagers/User.php
@@ -82,7 +82,7 @@ class User extends \Hubleto\Erp\RecordManager
         ifnull(' . $this->table . '.nick, ' . $this->table . '.email),
         if(' . $this->table . '.id = ' . $idUser .  ', " (you)", "")
       ) as _LOOKUP,
-      concat("rounded inline-block px-1 ", if(' . $this->table . '.id = ' . $idUser .  ', "bg-blue-100 text-blue-800", "bg-slate-100 text-slate-800")) as _LOOKUP_CLASS,
+      concat("rounded inline-block px-1 ", if(' . $this->table . '.id = ' . $idUser .  ', "border border-lime-300 bg-lime-50 text-lime-800", "border border-slate-100 bg-slate-50 text-slate-800")) as _LOOKUP_CLASS,
       "" as _LOOKUP_COLOR
     ');
 

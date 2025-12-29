613e06153f57cb39fceca1e0a6bdd8c8c37f6dd3
Dusan <dusan.daniska@wai.sk>
2025-12-29 19:35:22 +0100

    added system notifications on updated records

--- apps/Notifications/Sender.php
+++ apps/Notifications/Sender.php
@@ -33,7 +33,7 @@ class Sender extends Core
   ): array|bool {
     $idUser = $this->authProvider()->getUserId();
 
-    if ($idTo > 0) { //} && $idTo != $idUser) {
+    if ($idTo > 0 && $idTo != $idUser) {
       $mNotification = $this->getModel(Models\Notification::class);
       $notification = $mNotification->record->create([
         'priority' => $priority,

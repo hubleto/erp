74d953bc156a008b3e28d7287cef7a451ed80d18
Dusan <dusan.daniska@wai.sk>
2026-01-08 13:03:12 +0100

    IMAP and SMTP settings in mail account is not required

--- apps/Mail/Models/Account.php
+++ apps/Mail/Models/Account.php
@@ -6,6 +6,9 @@ use Hubleto\Framework\Db\Column\Integer;
 use Hubleto\Framework\Db\Column\Password;
 use Hubleto\Framework\Db\Column\Varchar;
 use Hubleto\Framework\Db\Column\Color;
+use Hubleto\Framework\Db\Column\Lookup;
+
+use Hubleto\App\Community\Auth\Models\User;
 
 use Hubleto\App\Community\Crypto\KeyManager;
 
@@ -17,6 +20,8 @@ class Account extends \Hubleto\Erp\Model
   public ?string $lookupSqlValue = '{%TABLE%}.name';
 
   public array $relations = [
+    'OWNER' => [ self::BELONGS_TO, User::class, 'id_owner', 'id'],
+    'MANAGER' => [ self::BELONGS_TO, User::class, 'id_manager', 'id'],
     'MAILBOXES' => [ self::HAS_MANY, Mailbox::class, 'id_account', 'id'],
   ];
 
@@ -25,6 +30,8 @@ class Account extends \Hubleto\Erp\Model
     return array_merge(parent::describeColumns(), [
       'name' => (new Varchar($this, $this->translate('Name')))->setRequired()->setCssClass('font-bold')->setIcon(self::COLUMN_NAME_DEFAULT_ICON),
       'color' => (new Color($this, $this->translate('Color')))->setIcon(self::COLUMN_COLOR_DEFAULT_ICON),
+      'id_owner' => (new Lookup($this, $this->translate('Owner'), User::class))->setReactComponent('InputUserSelect')->setDefaultValue($this->getService(\Hubleto\Framework\AuthProvider::class)->getUserId()),
+      'id_manager' => (new Lookup($this, $this->translate('Manager'), User::class))->setReactComponent('InputUserSelect')->setDefaultValue($this->getService(\Hubleto\Framework\AuthProvider::class)->getUserId()),
       'sender_email' => (new Varchar($this, $this->translate('Sender email address'))),
       'sender_name' => (new Varchar($this, $this->translate('Sender name'))),
       'imap_host' => (new Varchar($this, $this->translate('IMAP host'))),
--- apps/Mail/Models/RecordManagers/Account.php
+++ apps/Mail/Models/RecordManagers/Account.php
@@ -2,12 +2,25 @@
 
 namespace Hubleto\App\Community\Mail\Models\RecordManagers;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Hubleto\App\Community\Auth\Models\RecordManagers\User;
 
 class Account extends \Hubleto\Erp\RecordManager
 {
   public $table = 'mails_accounts';
 
+  /** @return BelongsTo<User, covariant Deal> */
+  public function OWNER(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'id_owner', 'id');
+  }
+
+  /** @return BelongsTo<User, covariant Lead> */
+  public function MANAGER(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'id_manager', 'id');
+  }
 
   /** @return HasMany<DealTask, covariant Deal> */
   public function MAILBOXES(): HasMany

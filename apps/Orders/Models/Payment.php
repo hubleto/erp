6a46bd97d5283860997d4da0fe414aaa7b89cb82
Dusan <dusan.daniska@wai.sk>
2026-01-16 06:28:09 +0100

    automated calculation of total payments in invoice

--- apps/Orders/Models/Payment.php
+++ apps/Orders/Models/Payment.php
@@ -40,7 +40,7 @@ class Payment extends \Hubleto\Erp\Model
       'discount' => new Decimal($this, $this->translate('Discount'))->setDefaultVisible()->setUnit('%'),
       'vat' => new Decimal($this, $this->translate('VAT'))->setUnit('%'),
       'notes' => (new Text($this, $this->translate('Notes')))->setDefaultVisible(),
-      'id_invoice_item' => (new Lookup($this, $this->translate('Invoice item'), Item::class))->setDefaultVisible()->setReadonly(),
+      'id_invoice_item' => (new Lookup($this, $this->translate('Invoice item'), Item::class))->setDefaultVisible(),
     ]);
   }
 

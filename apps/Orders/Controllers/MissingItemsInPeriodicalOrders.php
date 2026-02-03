06717f0bc815515f3c740cadff1bf59f6e1ff000
Dusan <dusan.daniska@wai.sk>
2026-02-03 09:21:46 +0100

    model Order.Payment replaced with Order.Item

--- apps/Orders/Controllers/MissingItemsInPeriodicalOrders.php
+++ apps/Orders/Controllers/MissingItemsInPeriodicalOrders.php
@@ -33,7 +33,19 @@ class MissingItemsInPeriodicalOrders extends \Hubleto\Erp\Controller
       ?->toArray()
     ;
 
+    $tmp = $mOrder->record
+      ->selectRaw('
+        orders.id,
+        ifnull(max(orders_items.date_due), "2000-01-01") as last_item_date_due
+      ')
+      ->leftJoin('orders_items', 'orders_items.id_order', '=', 'orders.id')
+      ->whereRaw('orders.payment_period > 0')
+      ->get()
+      ?->toArray()
+    ;
+
     $this->viewParams['orders'] = $orders;
+    $this->viewParams['tmp'] = $tmp;
 
     $this->setView('@Hubleto:App:Community:Orders/MissingItemsInPeriodicalOrders.twig');
 

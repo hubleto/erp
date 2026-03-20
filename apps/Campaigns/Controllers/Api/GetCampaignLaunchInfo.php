c6f4b82e224b85876aa37fe49148335aa9e1f6f1
Dusan <dusan.daniska@wai.sk>
2026-03-20 10:09:51 +0100

    campaignLaunchInfo: BOT_SCORE_GROUPED_BY_TIMESTAMP

--- apps/Campaigns/Controllers/Api/GetCampaignLaunchInfo.php
+++ apps/Campaigns/Controllers/Api/GetCampaignLaunchInfo.php
@@ -31,12 +31,13 @@ class GetCampaignLaunchInfo extends \Hubleto\Erp\Controllers\ApiController
       unset($recipients[$key]['MAIL']['body_text']);
       unset($recipients[$key]['MAIL']['body_html']);
 
-      $botScore = 0;
       $botScoreGroups = [];
-      foreach ($recipient->CLICKS as $click) {
-        $ts = round(strtotime($click->datetime_clicked) / 5000); // 5-second interval to group the clicks
-        if (!isset($botScoreGroups[$ts*5000])) $botScoreGroups[$ts*5000] = 0;
-        $botScoreGroups[$ts*5000] += $click->bot_score;
+      if (is_array($recipient['CLICKS'])) {
+        foreach ($recipient['CLICKS'] as $click) {
+          $ts = round(strtotime($click->datetime_clicked) / 5000); // 5-second interval to group the clicks
+          if (!isset($botScoreGroups[$ts*5000])) $botScoreGroups[$ts*5000] = 0;
+          $botScoreGroups[$ts*5000] += $click->bot_score;
+        }
       }
 
       $recipients[$key]['BOT_SCORE_GROUPED_BY_TIMESTAMP'] = $botScoreGroups;

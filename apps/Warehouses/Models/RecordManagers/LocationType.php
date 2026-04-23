766cf325e93d61adf2326461ff3439fca6460207
Dusan <dusan.daniska@wai.sk>
2026-04-23 17:19:26 +0200

    warehouses moved to enterprise apps

--- apps/Warehouses/Models/RecordManagers/LocationType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Hubleto\App\Community\Warehouses\Models\RecordManagers;
-
-class LocationType extends \Hubleto\Erp\RecordManager
-{
-  public $table = 'warehouses_locations_types';
-
-}
--- apps/Warehouses/Models/RecordManagers/WarehouseType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Hubleto\App\Community\Warehouses\Models\RecordManagers;
-
-class WarehouseType extends \Hubleto\Erp\RecordManager
-{
-  public $table = 'warehouses_types';
-
-}

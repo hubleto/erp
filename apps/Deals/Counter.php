f9989c1fac95979d3df4b5e6517775309faa1342
Dusan <dusan.daniska@wai.sk>
2026-06-18 17:14:01 +0200

    added deal and lead planning badges

--- apps/Deals/Counter.php
+++ apps/Deals/Counter.php
@@ -16,12 +16,18 @@ class Counter extends Core
   public function openDealsWithoutFuturePlan(): int
   {
     $mDeal = $this->getModel(Models\Deal::class);
+    $idUser = $this->authProvider()->getUserId();
+
     return $mDeal->record->prepareReadQuery()
       ->whereDoesntHave('ACTIVITIES', function($q) {
         $q->where('completed', false);
         $q->whereDate('date_start', '>=', date("Y-m-d"));
       })
       ->where($mDeal->table . '.is_closed', false)
+      ->where(function($q) use ($mDeal, $idUser) {
+        $q->where($mDeal->table . '.id_owner', $idUser);
+        $q->orWhere($mDeal->table . '.id_manager', $idUser);
+      })
       ->count()
     ;
 
--- apps/Leads/Counter.php
+++ apps/Leads/Counter.php
@@ -16,12 +16,18 @@ class Counter extends Core
   public function openLeadsWithoutFuturePlan(): int
   {
     $mLead = $this->getModel(Models\Lead::class);
+    $idUser = $this->authProvider()->getUserId();
+
     return $mLead->record->prepareReadQuery()
       ->whereDoesntHave('ACTIVITIES', function($q) {
         $q->where('completed', false);
         $q->whereDate('date_start', '>=', date("Y-m-d"));
       })
       ->where($mLead->table . '.is_closed', false)
+      ->where(function($q) use ($mLead, $idUser) {
+        $q->where($mLead->table . '.id_owner', $idUser);
+        $q->orWhere($mLead->table . '.id_manager', $idUser);
+      })
       ->count()
     ;
 

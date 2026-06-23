4e4ebf98960d45ab2be5becd9fc23c9b6de2d826
Dusan <dusan.daniska@wai.sk>
2026-06-23 08:09:22 +0200

    fix in recipients migration

--- apps/EmailMarketing/Models/Migrations/Recipient_0002.php
+++ apps/EmailMarketing/Models/Migrations/Recipient_0002.php
@@ -9,19 +9,26 @@ class Recipient_0002 extends Migration
 
   public function upgradeSchema(): void
   {
-    $this->db->execute("alter table `email_marketing_email_recipients` rename `email_marketing_recipients`");
+    $this->db->execute('alter table `email_marketing_recipients` add `id_campaign` int(8)');
+    $this->db->execute('alter table `email_marketing_recipients` add index(`id_campaign`)');
   }
 
   public function downgradeSchema(): void
   {
-    $this->db->execute("alter table `email_marketing_recipients` rename `email_marketing_email_recipients`");
+    $this->db->execute("alter table `email_marketing_recipients` drop `id_campaign`");
   }
 
   public function upgradeForeignKeys(): void
   {
+    $this->db->execute("
+      ALTER TABLE `email_marketing_recipients` ADD CONSTRAINT `fk__email_marketing_recipients__id_campaign` FOREIGN KEY (`id_campaign`)
+      REFERENCES `email_marketing_campaigns` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+    ");
   }
 
   public function downgradeForeignKeys(): void
   {
+    $this->db->execute("ALTER TABLE `email_marketing_recipients` DROP FOREIGN KEY `fk__email_marketing_recipients__id_campaign`;");
   }
+
 }
--- apps/EmailMarketing/Models/Migrations/Recipient_0003.php
+++ apps/EmailMarketing/Models/Migrations/Recipient_0003.php
@@ -9,26 +9,19 @@ class Recipient_0003 extends Migration
 
   public function upgradeSchema(): void
   {
-    $this->db->execute('alter table `email_marketing_recipients` add `id_campaign` int(8)');
-    $this->db->execute('alter table `email_marketing_recipients` add index(`id_campaign`)');
+    $this->db->execute("alter table `email_marketing_recipients` add `date_added` date");
   }
 
   public function downgradeSchema(): void
   {
-    $this->db->execute("alter table `email_marketing_recipients` drop `id_campaign`");
+    $this->db->execute("alter table `email_marketing_recipients` drop `date_added`");
   }
 
   public function upgradeForeignKeys(): void
   {
-    $this->db->execute("
-      ALTER TABLE `email_marketing_recipients` ADD CONSTRAINT `fk__email_marketing_recipients__id_campaign` FOREIGN KEY (`id_campaign`)
-      REFERENCES `email_marketing_campaigns` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-    ");
   }
 
   public function downgradeForeignKeys(): void
   {
-    $this->db->execute("ALTER TABLE `email_marketing_recipients` DROP FOREIGN KEY `fk__email_marketing_recipients__id_campaign`;");
   }
-
 }

8f680f86300de827a90812e5fac80ae2079612fb
Dusan <dusan.daniska@wai.sk>
2026-07-01 07:52:30 +0200

    updated periodicalOrdersMissingItems counter

--- apps/Orders/Counter.php
+++ apps/Orders/Counter.php
@@ -45,18 +45,18 @@ class Counter extends Core
   {
     $mOrder = $this->getModel(Models\Order::class);
 
-    $lastDayOfPreviousMonth = date("Y-m-t", strtotime("-2 month"));
+    // $lastDayOfPreviousMonth = date("Y-m-t", strtotime("-2 month"));
 
     return $mOrder->record
       ->prepareReadQuery()
       ->selectRaw('
         orders.id,
-        ifnull(max(orders_items.date_due), "2000-01-01") as last_item_date_due
+        ifnull(max(orders_items.charged_period_end), "2000-01-01") as last_charged_period_end
       ')
       ->leftJoin('orders_items', 'orders_items.id_order', '=', 'orders.id')
       ->groupBy('orders.id')
       ->whereRaw('orders.payment_period > 0 and ifnull(orders.is_closed, 0) = 0')
-      ->havingRaw('last_item_date_due <= "' . $lastDayOfPreviousMonth . '"')
+      ->havingRaw('last_charged_period_end <= now()')
       ->pluck('id')
       ?->toArray()
     ;

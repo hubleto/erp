189dec8addb3947e4a120184d23fc8a5d630d852
Dusan <dusan.daniska@wai.sk>
2026-07-31 13:12:41 +0200

    phase 10

--- apps/Leads/Components/FC/FormLead.tsx
+++ apps/Leads/Components/FC/FormLead.tsx
@@ -49,6 +49,13 @@ const TabDefault = () => {
   </>;
 };
 
+const TabTodo = () => <>TabTodo</>;
+const TabCalendar = () => <>TabCalendar</>;
+const TabEmailClicks = () => <>TabEmailClicks</>;
+const TabTasks = () => <>TabTasks</>;
+const TabHistory = () => <>TabHistory</>;
+const TabTimeline = () => <>TabTimeline</>;
+
 /**
  * FormLead
  *
@@ -64,56 +71,22 @@ const FormLead = (props: FormLeadProps) => {
     urlSlug='leads'
     // getContentClassName={(form: any): string => form.record.is_closed ? 'bg-slate-100' : ''}
     customEndpointParams={{saveRelations: ['TAGS'] }}
-    getTabs={(form: any) => [
-      { uid: 'default', title: <b>{translate('Lead')}</b> },
-      { uid: 'todo', title: translate('Todo') },
-      { uid: 'calendar', title: translate('Calendar') },
-      { uid: 'email_clicks', title: translate('Email Clicks') },
-      { uid: 'tasks', title: translate('Tasks'), showCountFor: 'TASKS' },
-      { uid: 'history', icon: 'fas fa-clock-rotate-left', position: 'right' },
-      { uid: 'timeline', icon: 'fas fa-timeline', position: 'right' },
-      ...form.getCustomTabs()
-    ]}
-
     showWorkflowUi={true}
     showOwnerManagerUi={true}
 
     uiComponents={{
       title: () => <Title />,
       tabs: {
-        default: {
-          title: <b>{translate('Lead')}</b>,
-          content: () => <TabDefault />,
-        },
-        todo: {
-          title: translate('Todo'),
-          content: () => <div>Tab: Todo</div>,
-        },
-        calendar: {
-          title: translate('Calendar'),
-          content: () => <div>Tab: Calendar</div>,
-        },
-        email_clicks: {
-          title: translate('Email Clicks'),
-          content: () => <div>Tab: Email Clicks</div>,
-        },
-        tasks: {
-          title: translate('Tasks'),
-          showCountFor: 'TASKS',
-          content: () => <div>Tab: Tasks</div>,
-        },
-        history: {
-          icon: 'fas fa-clock-rotate-left',
-          position: 'right',
-          content: () => <div>Tab: History</div>,
-        },
-        timeline: {
-          icon: 'fas fa-timeline',
-          position: 'right',
-          content: () => <div>Tab: Timeline</div>,
-        },
+        default: { title: <b>{translate('Lead')}</b>, content: () => <TabDefault /> },
+        todo: { title: translate('Todo'), content: () => <TabTodo /> },
+        calendar: { title: translate('Calendar'), content: () => <TabCalendar /> },
+        email_clicks: { title: translate('Email Clicks'), content: () => <TabEmailClicks /> },
+        tasks: { title: translate('Tasks'), content: () => <TabTasks /> },
+        history: { icon: 'fas fa-clock-rotate-left', position: 'right', content: () => <TabHistory /> },
+        timeline: { icon: 'fas fa-timeline', position: 'right', content: () => <TabTimeline /> },
       },
-      
+      // saveButton: () => <div>save</div>,
+      // closeButton: () => <div>close</div>,
     }}
 
   ></Form>;

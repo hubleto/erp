667ee8afe6908b9cac94d04069a1240efaf28e32
Dusan <dusan.daniska@wai.sk>
2026-08-19 15:55:56 +0200

    phase 58

--- apps/Projects/Loader.php
+++ apps/Projects/Loader.php
@@ -111,11 +111,12 @@ class Loader extends \Hubleto\Erp\App
   public function renderSecondSidebar(): string
   {
     return '
-      <div class="flex flex-col gap-2">
-        <a class="btn btn-square btn-primary-outline" href="' . $this->env()->projectUrl . '/projects">
-          <span class="icon"><i class="fas fa-diagram-project"></i></span>
-          <span class="text">' . $this->translate('Projects') . '</span>
+      <div class="app-main-title">
+        <a href="' . $this->env()->projectUrl . '/projects">
+          ' . $this->translate('Projects') . '
         </a>
+      </div>
+      <div class="app-sidebar-buttons">
         <a class="btn btn-transparent" href="' . $this->env()->projectUrl . '/projects/milestones">
           <span class="icon"><i class="fas fa-calendar-check"></i></span>
           <span class="text">' . $this->translate('Milestones') . '</span>

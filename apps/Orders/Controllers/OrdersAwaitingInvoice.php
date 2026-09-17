9d6d6a58a74e8db66838dd1b85485b102af39346
Dusan <dusan.daniska@wai.sk>
2026-09-17 08:04:18 +0200

    ui improvements

--- apps/Orders/Controllers/OrdersAwaitingInvoice.php
+++ apps/Orders/Controllers/OrdersAwaitingInvoice.php
@@ -10,7 +10,6 @@ class OrdersAwaitingInvoice extends \Hubleto\Erp\Controller
   public function getBreadcrumbs(): array
   {
     return array_merge(parent::getBreadcrumbs(), [
-      [ 'url' => 'orders', 'content' => $this->translate('Orders') ],
       [ 'url' => '', 'content' => $this->translate('Orders awaiting invoice') ],
     ]);
   }

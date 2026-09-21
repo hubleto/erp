2142551f30b3f3d49f402d7a1efdbfa8b349a690
Dusan <dusan.daniska@wai.sk>
2026-09-21 09:49:25 +0200

    error handling fixing

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -23,6 +23,7 @@ class ApiController extends \Hubleto\Erp\Controller
       return [
         'status' => 'error',
         'code' => $e->getCode(),
+        'trace' => $e->getTraceAsString(),
         'message' => $e->getMessage(),
       ];
     }
